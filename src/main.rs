2dd36bbcf8be7d8ab79e5230827ae70349526191
Jacob Haig <jhaig343@gmail.com>
2019-08-09 23:36:04 -0400

    make new shell main + setup basic command structure

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,27 @@
-// structopt is useful for building CLI applications
+// Rust SHell
+// v.0.1.0
+// By Jacob Haig (jhaig343@gmail.com)
+
 use structopt::StructOpt;
 use failure::ResultExt;
 use exitfailure::ExitFailure;
-
+use std::path::PathBuf;
 
 #[derive(StructOpt, Debug)]
-struct Cli {
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+#[structopt(name = "rush", about = "Rust Shell command structure.")]
+struct Args {
+
+	command: String, //inital cli command
+	#[structopt(parse(from_os_str))]
+	params: Vec<PathBuf> //command arguments(any number)
 }
 
 
-
-impl std::fmt::Display for Cli {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "file: {}", self.path.to_string_lossy())
-    }
-}
-
+// shell entrypoint
 fn main() -> Result<(), ExitFailure> {
-    let args = Cli::from_args();
-    // read in the file contents (as one big string...)
-    // question mark on end acts like commented match block below(easy way to catch errors)
-    let content = std::fs::read_to_string(&args.path).with_context(|_| format!("Error reading {}", &args.path.to_string_lossy()))?;
-    // let result = match content {
-        // Ok(_content) => { _content },
-        // Err(error) => { panic!("An Error occurred: {}", error);}
-    // };
-    println!("file content: {}", content );
-    Ok(())
-    // println!("{:?}", content);
-    // echo back line with the pattern being searched for
-    // FIXME: this doesn't gel with new Result content type.
-    // for line in content.lines() {
-    //     if line.contains(&args.pattern) {
-    //         println!("{}", line);
-    //     }
-    // }
+	let args = Args::from_args();
+	let command = args.command;
+	let params = args.params;
+	println!("{}", command);
+	Ok(())
 }
